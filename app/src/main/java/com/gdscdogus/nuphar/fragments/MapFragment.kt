4725b348ecb626d252ff9b1739a830900fa3b892
Şevket Eren KAYNAR <sqayner@gmail.com>
2024-02-24 17:28:25 +0300

    map added

--- app/src/main/java/com/gdscdogus/nuphar/fragments/MapFragment.kt
+++ app/src/main/java/com/gdscdogus/nuphar/fragments/MapFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.gdscdogus.nuphar.databinding.FragmentHomeBinding
 import com.gdscdogus.nuphar.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
     lateinit var binding: FragmentMapBinding
+    lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +21,41 @@ class MapFragment : Fragment() {
     ): View {
         binding = FragmentMapBinding.inflate(inflater)
 
+        binding.mapView.onCreate(savedInstanceState);
+        binding.mapView.getMapAsync(this);
+
         return binding.root
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        map = p0;
+        map.uiSettings.isMyLocationButtonEnabled = false;
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
+            com.google.android.gms.maps.model.LatLng(
+                41.0015935038339,
+                29.176088384635502
+            ), 18F
+        )
+        map.animateCamera(cameraUpdate)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
