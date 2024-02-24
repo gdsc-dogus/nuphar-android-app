124a7d0dcb54e266b3a99825412b68ec62eaec0b
Şevket Eren KAYNAR <sqayner@gmail.com>
2024-02-25 01:33:15 +0300

    big update

--- app/src/main/java/com/gdscdogus/nuphar/fragments/HomeFragment.kt
+++ app/src/main/java/com/gdscdogus/nuphar/fragments/HomeFragment.kt
@@ -29,8 +29,19 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Global Environmental Solutions Summit: Uniting for a Sustainable Future",
+                "Participants will convene online to explore a range of environmental topics, including climate change, biodiversity loss, pollution, and resource depletion. Through keynote speeches, panel discussions, and interactive workshops, attendees will deepen their understanding of these issues and exchange ideas on effective strategies for mitigation and adaptation.",
+                "https://meet.google.com/ibq-rzqp-dzj",
+                CoordinatesModel(2.2, 2.2),
+                "id",
+                Instant.now()
+            )
+        )
+        events.add(
+            EventModel(
+                "id",
+                "Orchard Community Day: Harvest and Share",
+                "During the event, participants will gather at an orange orchard to collectively pick or plant oranges. In this process, participants will not only spend time in a natural environment but also gain experience in agricultural activities. Additionally, they will learn about the techniques used in orange harvesting or planting.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
@@ -40,8 +51,8 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Watershed Cleanup Day: Unite for Cleaner Rivers",
+                "Participants will gather at designated water bodies to remove trash, debris, and pollutants from the environment. By working together to clean up our watersheds, participants will not only contribute to a healthier ecosystem but also gain firsthand experience in environmental conservation efforts.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
@@ -51,8 +62,8 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Desert Restoration Rally: Taking Action Against Desertification",
+                "Participants will gather at designated desertification hotspots, where they will engage in activities such as tree planting, soil conservation, and erosion control. Through teamwork and collective effort, attendees will contribute to the revitalization of degraded landscapes, helping to restore biodiversity, conserve water resources, and improve soil health.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
@@ -62,8 +73,8 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Forest Regeneration Initiative: Community Action for Sustainable Forest Management",
+                "Participants will gather at designated forest sites, where they will collaborate on a variety of restoration and conservation projects. These activities may include tree planting, invasive species removal, trail maintenance, and habitat restoration. Through teamwork and collective effort, participants will contribute to the regeneration of forested areas, fostering biodiversity, improving soil quality, and mitigating the impacts of climate change.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
@@ -73,8 +84,8 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Clean Water Campaign: Community Action for Safe Drinking Water",
+                "Participants will gather at various locations within the community to engage in a range of activities aimed at improving access to safe drinking water. These activities may include water testing, source protection initiatives, infrastructure improvements, and public awareness campaigns. Through collaborative efforts, participants will work towards the goal of ensuring that everyone has access to clean and safe drinking water.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
@@ -84,30 +95,8 @@ class HomeFragment : Fragment() {
         events.add(
             EventModel(
                 "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
-                "adres",
-                CoordinatesModel(2.2, 2.2),
-                "id",
-                Instant.now()
-            )
-        )
-        events.add(
-            EventModel(
-                "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
-                "adres",
-                CoordinatesModel(2.2, 2.2),
-                "id",
-                Instant.now()
-            )
-        )
-        events.add(
-            EventModel(
-                "id",
-                "Lorem ipsum dolor sit amet",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec diam lorem. In vel vulputate ex. Duis quis dui ut magna pretium facilisis a quis lorem. Aenean iaculis sapien vitae sollicitudin.",
+                "Wastewater Guardians: Protecting Community Health through Safe Wastewater Management",
+                "Participants will gather at key sites within the community to engage in a variety of activities aimed at promoting safe wastewater management. These activities may include conducting water quality tests, identifying sources of pollution, implementing pollution prevention measures, and advocating for policy changes to improve wastewater treatment infrastructure. By working collaboratively, participants will contribute to the protection of water bodies and ecosystems from the harmful impacts of wastewater contamination.",
                 "adres",
                 CoordinatesModel(2.2, 2.2),
                 "id",
--- app/src/main/java/com/gdscdogus/nuphar/fragments/MapFragment.kt
+++ app/src/main/java/com/gdscdogus/nuphar/fragments/MapFragment.kt
@@ -1,14 +1,26 @@
 package com.gdscdogus.nuphar.fragments
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Canvas
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.gdscdogus.nuphar.R
 import com.gdscdogus.nuphar.databinding.FragmentMapBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     lateinit var binding: FragmentMapBinding
@@ -29,14 +41,48 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         map = p0;
-        map.uiSettings.isMyLocationButtonEnabled = false;
+        map.uiSettings.isMyLocationButtonEnabled = false
+
+        val dogusUniversity = com.google.android.gms.maps.model.LatLng(
+            41.0015935038339,
+            29.176088384635502
+        )
+
         val cameraUpdate = CameraUpdateFactory.newLatLngZoom(
-            com.google.android.gms.maps.model.LatLng(
-                41.0015935038339,
-                29.176088384635502
-            ), 18F
+            dogusUniversity, 18F
         )
         map.animateCamera(cameraUpdate)
+
+        val markerOptions = MarkerOptions();
+        markerOptions.position(dogusUniversity);
+        markerOptions.title("DOGUS University")
+        markerOptions.icon(
+            BitmapFromVector(
+                requireContext(),
+                R.drawable.ic_location_24dp
+            )
+        );
+        map.addMarker(markerOptions);
+    }
+
+    private fun BitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(
+            context, vectorResId
+        )
+        vectorDrawable!!.setBounds(
+            0, 0, vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+        vectorDrawable.colorFilter = BlendModeColorFilter(context.getColor(R.color.green_4), BlendMode.SRC_ATOP)
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
     override fun onResume() {
